f2db8d16e6876deafd5c8dec7aaa3c42b15a2c6a
EduardoPicolo <eduardonpicolo@gmail.com>
2019-10-05 17:48:13 -0300

    Reserva de spots funcionando

--- mobile/mobile/src/components/SpotList.js
+++ mobile/mobile/src/components/SpotList.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { withNavigation } from 'react-navigation';
 import { View, Text, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native';
 
 import api from '../services/api';
 
-export default function SpotList({ tech }){
+function SpotList({ tech, navigation }){
     const [spots, setSpots] = useState([]);
 
     useEffect(() => {
@@ -15,6 +16,10 @@ export default function SpotList({ tech }){
         }
         LoadSpots();
     }, []);
+
+    function handleNavigate(id){
+        navigation.navigate('Book', { id });
+    }
 
     return (
         <View style={styles.container}> 
@@ -31,8 +36,8 @@ export default function SpotList({ tech }){
                         <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url}} />
                         <Text style={styles.company}>{item.company}</Text>
                         <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : 'GRATUITO'}</Text>
-                        <TouchableOpacity onPress={() => {}} style={styles.button}>
-                            <Text style={styles.buttonText}>Solicitar reserva</Text>
+                        <TouchableOpacity onPress={() => handleNavigate(item._id)} style={styles.button}>
+                            <Text style={styles.buttonText}>Solicitar reserva</Text> 
                         </TouchableOpacity>
                     </View>
                 )}
@@ -102,3 +107,5 @@ const styles = StyleSheet.create({
         fontSize: 15,
     },
 });
+
+export default withNavigation(SpotList);
--- mobile/mobile/src/pages/Book.js
+++ mobile/mobile/src/pages/Book.js
@@ -1,6 +1,94 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Alert, SafeAreaView, AsyncStorage, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function Book() {
-    return <View />
+import api from '../services/api';
+
+export default function Book({ navigation }) {
+    const [date, setDate] = useState('');
+    const id = navigation.getParam('id');
+
+    async function handleSubmit(){
+        const user_id = await AsyncStorage.getItem('user');
+
+        await api.post(`/spots/${id}/bookings`, {
+            date
+        }, {
+            headers: {user_id}
+        })
+
+        Alert.alert('Solicitação de reserva enviada.');
+
+        navigation.navigate('List');
+    }
+
+    function handleCancel(){
+        navigation.navigate('List');
+    }
+
+    return (
+        <SafeAreaView style={styles.container}> 
+            <Text style={styles.label}>DATA DE INTERESSE</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Data"
+                placeholderTextColor='#999'
+                autoCapitalize="words"
+                autoCorrect={false}
+                value={date}
+                onChangeText={setDate}
+            />
+
+            <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+                <Text styles={styles.buttonText}>Solicitar reserva</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={handleCancel} style={[styles.button, styles.cancelButton]}> 
+                <Text styles={styles.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
+
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 40,
+        margin: 30,
+    },
+
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2,
+    },
+
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+
+    cancelButton: {
+        marginTop: 10,
+        backgroundColor: '#ccc',
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
